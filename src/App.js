d4a9624bec0e39d01a2a24c41dc4808b2ffdbab4
zainikhan999 <zk.032918@gmail.com>
2024-10-05 17:43:14 +0500

    project uploaded

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import React, { useState } from 'react';
+import './App.css';
+import './index.css';
+import { list } from 'postcss';
+function App() {
+   const [title,setTitle]=useState(" ")
+   const [desc,setdesc]=useState(" ")
+   const [mainTask,setMainTask]=useState([])
+      const submitHandler=(e)=>
+   {
+    e.preventDefault();
+
+    setMainTask([...mainTask,{title , desc}]);
+    setTitle("");
+    setdesc("");
+    console.log(mainTask)
+    
+   };
+
+   const deleteHandler=(i)=>
+   {
+      let copyTask=[...mainTask]
+      copyTask.splice(i,1)
+      setMainTask(copyTask)
+
+
+   }
+
+  
+   let renderTask = <h2>No task available</h2>;
+
+    if (mainTask.length>0)
+    {
+      renderTask = mainTask.map((t, i) => {
+       
+          return (
+          <li key={i} className='flex items-center justify-around'>
+          {/* console.log({i} {t.title} {t.desc}) */}
+          <div className='flex justify-between w-2/3'>
+          <h5 className='text-xl font-semibold'>{t.title}</h5>
+          <h6 className='text-xl font-medium'>{t.desc}</h6>
+          </div>
+        <button className='bg-red-500 text-white px-4 py-2 rounded font-bold mb-2'
+        onClick={()=>
+        {
+          deleteHandler(i)
+        }
+        }
+        
+        
+        >Delete</button>
+        
+        </li>)}
+        )
+     
+    
+    }
+ 
+   return(
+    <>
+    < h1 className='bg-black text-white text-center p-5 text-5xl'>
+     Todolist 
+    </h1>
+    <form onSubmit={submitHandler}>
+      <input
+      type="text"  
+      className="text border-zinc-800 border-2 m-5 px-6 py-2"
+      placeholder="Enter task here" 
+      value={title}
+      onChange={(e)=>
+      {
+        setTitle(e.target.value)
+
+      }
+      }
+     />
+
+     <input
+      type="text"  
+      className="text border-zinc-800 border-2 m-5 px-6 py-2"
+      placeholder="Enter Description here"
+      value={desc}
+      onChange={(e)=>
+      {setdesc(e.target.value)}
+      }
+     />
+
+     <button className='bg-black text-white rounded px-4 py-2 '>
+      Add Task
+     </button>
+     
+    </form>
+
+    <hr />
+
+    <div className='p-9 bg-slate-200'>
+      <ul>
+        {renderTask}
+      </ul>
+   </div>
+    </>
+   )
+  
+  
+}
+
+export default App;
